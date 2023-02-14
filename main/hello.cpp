7c25dc68ca288b2804c3cd591fecd1f4988e10b9
Neeraj <neerajperso@gmail.com>
2023-02-14 09:36:09 +0530

    test

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "PES1UG21CS824 : Neeraj Gopalakrishnan";
+  cout << "PES1UG21CS824 : Neeraj Gopalakrishnan\n";
   return 0;
 }
